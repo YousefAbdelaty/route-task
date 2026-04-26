91b4df6cd09a5c2b33fe7fa3ab50c453618917e3
Yousef Abdelaty <yossifabdelaty@gmail.com>
2026-04-26 21:03:54 +0300

    completed most of issue-details page

--- src/app/features/issues/pages/issue-details/issue-details.component.ts
+++ src/app/features/issues/pages/issue-details/issue-details.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { IssuesService } from '../../../../core/issues.service';
+import { ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-issue-details',
   standalone: true,
-  imports: [],
+  imports: [FormsModule , CommonModule],
   templateUrl: './issue-details.component.html',
   styleUrl: './issue-details.component.css'
 })
 export class IssueDetailsComponent {
+  constructor(private issueService:IssuesService , private route:ActivatedRoute){}
+  post:any;
+  comments:any[]=[];
+  ngOnInit(){
+    this.route.paramMap.subscribe(params => {
+      const id = Number (params.get('id'));
+      if(id){
+        this.issueService.getPostById(id).subscribe(data=>{
+          this.post = data;
+        });
+        this.issueService.getComments(id).subscribe(data=>{
+          this.comments = data;
+        });
+      }
+    });
+  }
 
 }
